c5e79366ea36552d5b685aad793e2917f5f0f428
Lawrence Zheng <ruizheng1119@gmail.com>
2019-04-28 23:28:04 +0800

    git init, create vue Django project, install element and axios, finish view of Login

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        AI 面试官
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<style lang="scss">
+  .el-header{
+    height: 60px;
+    line-height: 60px;
+    font-size: 20px;
+    background-color: #fff;
+    border-bottom: 1px solid #DCDFE6;
+  }
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <el-row align="middle">
+      <el-col :span="8" :offset="8">
+        <el-form ref="loginForm" :model="loginForm" :rules="loginRules" label-width="80px" label-position="left">
+          <el-form-item label="用户名" prop="name">
+            <el-input v-model="loginForm.name" placeholder="请输入用户名" prefix-icon=""></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="loginForm.password" placeholder="请输入密码" prefix-icon=""></el-input>
+          </el-form-item>
+          <el-form-item label="面试码" prop="interview_code">
+            <el-input v-model="loginForm.interview_code" placeholder="请输入面试码" prefix-icon=""></el-input>
+          </el-form-item>
+          <el-form-item label="登陆身份" prop="identity">
+            <el-radio-group v-model="loginForm.identity" prop="identity">
+              <el-radio label="求职者"></el-radio>
+              <el-radio label="面试官"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-form>
+        <el-button class="login-button" @click="submitForm('loginForm')">登陆</el-button>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Login",
+      data(){
+          return{
+            loginForm:{
+              name:'',
+              password:'',
+              interview_code:'',
+              identity:'',
+            },
+            loginRules:{
+              name:[
+                {required:true,message:'请输入用户名',trigger:'blur'}
+              ],
+              password:[
+                {required:true,message:'请输入密码',trigger:'blur'}
+              ],
+              interview_code:[
+                {required:true,message:'请输入面试码',trigger:'blur'}
+              ],
+              identity:[
+                {required:true,message:'请选择身份',trigger:'change'}
+              ],
+            }
+          }
+      },
+      methods:{
+        submitForm(formName){
+          this.$refs[formName].validate((valid)=>{
+            if(valid){
+              this.$message({
+                message:'验证成功',
+                type:'success'
+              });
+            }
+            else {
+              this.$message({
+                message:'验证失败',
+                type:'error'
+              });
+            }
+          })
+        }
+      }
+
+    }
+</script>
+
+<style scoped>
+  .login-button{
+    width: 100%;
+  }
+</style>
